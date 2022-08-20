9f9184d72441dd16dceea7832383821f5e763460
Avinash Jayakar <avithemad@Dhananjayarajs-MacBook-Pro.local>
2022-08-20 12:14:41 +0530

    Refactor qr code scanner

--- src/components/Qrscanner.js
+++ src/components/Qrscanner.js
@@ -5,7 +5,10 @@ function Qrscanner() {
   const reader = new BrowserMultiFormatReader();
   const [devices, setDevices] = React.useState([]);
   const [selectedDevice, setSelectedDevice] = React.useState(0);
+  const [qrResult, setQrResult] = React.useState("");
+  const [displayVideo, setDisplayVideo] = React.useState(false);
   const scanQr = (ev) => {
+    setDisplayVideo(true);
     reader.listVideoInputDevices().then((devices) => {
       setDevices(devices);
       if (devices) {
@@ -14,7 +17,8 @@ function Qrscanner() {
           "video",
           (result, err) => {
             if (result) {
-              console.log(result);
+              setQrResult(result.text);
+              setDisplayVideo(false);
               reader.reset();
             }
           }
@@ -23,21 +27,56 @@ function Qrscanner() {
     });
   };
   const changeCamera = (ev) => {
-    console.log(selectedDevice);
+    reader.reset();
+    if (devices) {
+      reader.decodeFromVideoDevice(
+        devices[(selectedDevice + 1) % devices.length].id,
+        "video",
+        (result, err) => {
+          if (result) {
+            setQrResult(result.text);
+            setDisplayVideo(false);
+            reader.reset();
+          }
+        }
+      );
+    }
     setSelectedDevice((selectedDevice + 1) % devices.length);
   };
 
   return (
-    <div>
-      <div>{JSON.stringify(devices)}</div>
-      This is qrscanner section
-      <video id="video"></video>
+    <div className="w-9/12 mx-auto ">
+      <h1 className="text-xl mt-4">Scan and upload file movement</h1>
+      <ol className="ml-4 mt-4">
+        <li className="list-disc">Allow access to camera</li>
+        <li className="list-disc">Click Scan to start the camera</li>
+        <li className="list-disc">
+          Once the camera detects the QR, it is validated and new form is
+          presented
+        </li>
+        <li className="list-disc">Fill in the form and click Submit.</li>
+      </ol>
+      <p className="mt-4">
+        Note: Use the Change camera button to change camera in mobile devices.
+      </p>
       <div>
-        <button onClick={scanQr}>Scan QR</button>
+        <button
+          className="mr-2 my-4 bg-green-600 px-2 py-1 rounded"
+          onClick={scanQr}
+        >
+          Scan QR
+        </button>
         {devices.length > 0 && (
-          <button onClick={changeCamera}>Change camera</button>
+          <button
+            className="mr-2 my-4 bg-green-600 px-2 py-1 rounded"
+            onClick={changeCamera}
+          >
+            Change camera
+          </button>
         )}
       </div>
+      {displayVideo && <video id="video" height={300} width={200}></video>}
+      <div>{qrResult}</div>
     </div>
   );
 }
